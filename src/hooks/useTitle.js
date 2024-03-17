d1d1b19eb5fb3a2dd23e90cdfa77edf07ecafafe
DesalegnSendek <desalegnsendek2@gmail.com>
2024-03-18 01:23:26 +0300

    name modified

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export const useTitle = (title) => {
 
     useEffect(() => {
-        document.title = `${title} / Movielia`;
+        document.title = `${title} / dESS_movies`;
     });
 
   return null;
